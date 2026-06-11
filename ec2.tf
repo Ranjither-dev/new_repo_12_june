d4be3b73481a73df20e41f892665b7618eea6fb6
ranjith <ranji_7@blah-key.localdomain>
2026-06-11 21:41:48 +0530

    push from prod branch

--- ec2.tf
+++ ec2.tf
@@ -1,13 +1,4 @@
-resource "aws_instance" "pub_server" {
-  ami                     = "ami-0aa31b568c1e8d622"
-  instance_type           = "t3.micro"
-  vpc_security_group_ids = [aws_security_group.public_sg.id]
-  subnet_id                = aws_subnet.public_subnet.id
-  key_name  = "hyd_pem" 
-
-  tags = { 
-
-    Name = "pub_server" 
-  }
-
-}
+hi hello vanakkan da mapla ....
+ulla file edhum irukadhu ....
+en na idhu dummy file ...
+prod branch la testig kaaga upload pannathu ...
